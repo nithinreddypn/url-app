53f3df810e2244c36dac8675fcddc0c95073cb04
Nova <nithinreddy7363@gmail.com>
2026-07-24 19:51:20 +0530

    Update Google client ID to the new Android-authorized credentials

--- lib/services/google_sign_in_service_native.dart
+++ lib/services/google_sign_in_service_native.dart
@@ -13,7 +13,7 @@ class GoogleSignInService {
   static const webClientId = String.fromEnvironment(
     'GOOGLE_WEB_CLIENT_ID',
     defaultValue:
-        '729107585198-2djt2sm2nho5k52cvc0itom0m1nmrkpa.apps.googleusercontent.com',
+        '729107585198-ejc73mmf2uu19jvo70nseq9ujcsnrqpm.apps.googleusercontent.com',
   );
 
   final GoogleSignIn _googleSignIn = GoogleSignIn.instance;
